8105ced893ff601393d0c724465e58ea57bc48be
Wesley Adam Hall <ha0406@ad.unc.edu>
2023-05-04 00:38:32 -0400

    add endpoints and check

--- server.js
+++ server.js
@@ -1,5 +1,63 @@
 import { rps } from "./lib/rpsls.js";
 import { rpsls } from "./lib/rpsls.js";
 import minimist from "minimist";
+import express from "express";
 
 const args = minimist(process.argv.slice(2));
+
+const port = args.port || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// app endpoint
+app.get("/app/", (req, res) => {
+  res.status(200).send('200 OK');
+})
+
+// app rps and rpsls
+app.get("/app/rps/", (req, res) => {
+  res.status(200).send(JSON.stringify(rps()));
+})
+
+app.get("/app/rpsls/", (req, res) => {
+  res.status(200).send(JSON.stringify(rpsls()));
+})
+
+// /play/ rps and rpsls QUERY
+app.get("/app/rps/play/", (req, res) => {
+  res.status(200).send(JSON.stringify(rps(req.query.shot)));
+})
+
+app.get("/app/rpsls/play/", (req, res) => {
+  res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
+})
+
+// /play/ rps and rpsls BODY
+app.get("/app/rps/play/", (req, res) => {
+  res.status(200).send(JSON.stringify(rps(req.body.shot)));
+})
+
+app.get("/app/rpsls/play/", (req, res) => {
+  res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+})
+
+// :shot rps and rpsls PARAMS
+app.get("/app/rps/play/:shot", (req, res) => {
+  res.status(200).send(JSON.stringify(rps(req.params.shot)));
+})
+
+app.get("/app/rpsls/play/:shot", (req, res) => {
+  res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+})
+
+// 404 status
+app.get("*", (req, res) => {
+  res.status(404).send("404 NOT FOUND");
+})
+
+// listen on port
+app.listen(port, () => {
+  console.log('Server listening on port ' + port);
+})
